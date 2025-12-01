6719f84d6d08b7ea0bbeb095616d1dc64805c6e4
HeenaPanchal <heenapanchal2921@gmail.com>
2025-12-01 21:11:31 +0530

    change into script

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const joke = document.getElementById("joke");
 const btn = document.getElementById("btn");
-console.log(btn);
-console.log(joke);
 const url = "https://v2.jokeapi.dev/joke/Any?type=single";
 
 const getJoke = async () => {
